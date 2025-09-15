f1d8c22509d698e3b157f8c21c12f3b9cb47d4a4
chicken-face <8035612@student.harmonytx.org>
2025-09-15 16:37:41 +0000

    Show error if iframe is blocked by security headers

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function generateIframe() {
     alert('Please enter a URL.');
     return;
   }
-  // Remove any previous iframe
+  // Remove any previous iframe or error message
   var oldIframe = document.getElementById('bypass-iframe');
   if (oldIframe) {
     oldIframe.remove();
+  }
+  var oldError = document.getElementById('iframe-error');
+  if (oldError) {
+    oldError.remove();
   }
   // Create new iframe
   var iframe = document.createElement('iframe');
@@ -17,5 +21,14 @@ function generateIframe() {
   iframe.height = '600';
   iframe.style.border = '2px solid #2d7dd2';
   iframe.style.marginTop = '20px';
+  // Show error if iframe fails to load (blocked by X-Frame-Options or CSP)
+  iframe.onerror = function() {
+    var errorMsg = document.createElement('div');
+    errorMsg.id = 'iframe-error';
+    errorMsg.style.color = 'red';
+    errorMsg.style.marginTop = '20px';
+    errorMsg.textContent = 'This site cannot be loaded in an iframe due to security restrictions.';
+    document.body.appendChild(errorMsg);
+  };
   document.body.appendChild(iframe);
 }
